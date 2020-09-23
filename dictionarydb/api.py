ffbcc41d2f6c270ee2c9bf00cf1a488cfc38c922
Markus Kaiserswerth <github@sensun.org>
2020-09-23 18:12:52 +0200

    Use SQLite query as standard SQL query

--- dictionarydb/api.py
+++ dictionarydb/api.py
@@ -29,6 +29,48 @@ async def on_shutdown():
         await database.disconnect()
 
 
+LOOKUP_QUERY = """
+with words_in_request_language as (
+    select *
+    from word
+    where language_id = (
+        select id from language
+        where code = :source_language
+    )
+), words_matching_search as (
+    select *
+    from words_in_request_language
+    where text like :search_string || '%'
+), words_with_translation_ids as (
+    select words.*, translations.*
+    from words_matching_search as words
+    inner join word_translates_to_word as translations
+    on (words.id in (translations.word1_id, translations.word2_id))
+), words_with_translations as (
+    select distinct words.language_id as language_id,
+                    words.text as word,
+                    translated.language_id as translation_language_id,
+                    translated.text as translation
+    from words_with_translation_ids words
+    inner join word as translated
+    on (translated.id in (words.word1_id, words.word2_id)
+        and words.language_id != translated.language_id
+        and translated.language_id = (select id from language where code = :target_language))
+), results_with_languages as (
+    select words.word,
+           (select code
+           from language
+           where id = words.language_id) as language,
+           words.translation,
+           (select code
+           from language
+           where id = words.translation_language_id) as translation_language
+    from words_with_translations words
+)
+select * from results_with_languages
+limit :max_results
+"""  # noqa
+
 LOOKUP_QUERY_POSTGRESQL = """
 with words_in_request_language as (
     select *
@@ -76,56 +118,14 @@ select * from results_by_relevance
 limit :max_results
 """  # noqa
 
-LOOKUP_QUERY_SQLITE = """
-with words_in_request_language as (
-    select *
-    from word
-    where language_id = (
-        select id from language
-        where code = :source_language
-    )
-), words_matching_search as (
-    select *
-    from words_in_request_language
-    where text like :search_string || '%'
-), words_with_translation_ids as (
-    select words.*, translations.*
-    from words_matching_search as words
-    inner join word_translates_to_word as translations
-    on (words.id in (translations.word1_id, translations.word2_id))
-), words_with_translations as (
-    select distinct words.language_id as language_id,
-                    words.text as word,
-                    translated.language_id as translation_language_id,
-                    translated.text as translation
-    from words_with_translation_ids words
-    inner join word as translated
-    on (translated.id in (words.word1_id, words.word2_id)
-        and words.language_id != translated.language_id
-        and translated.language_id = (select id from language where code = :target_language))
-), results_with_languages as (
-    select words.word,
-           (select code
-           from language
-           where id = words.language_id) as language,
-           words.translation,
-           (select code
-           from language
-           where id = words.translation_language_id) as translation_language
-    from words_with_translations words
-)
-select * from results_with_languages
-limit :max_results
-"""  # noqa
 
-
-def get_lookup_query(database_name):
+def get_lookup_query(database_name=None):
     if database_name == "postgresql":
         return LOOKUP_QUERY_POSTGRESQL
-    elif database_name == "sqlite":
-        return LOOKUP_QUERY_SQLITE
     else:
-        raise NotImplementedError("unsupported database")
+        return LOOKUP_QUERY
+
+
 
 
 DEFAULT_NUM_RESULTS = 20
