d645eae9eb93f601e6467a4192b297d89c2d850d
Markus Kaiserswerth <github@sensun.org>
2020-10-05 12:40:06 +0200

    Add missing assertions for API host and port defaults

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,4 +6,6 @@ def test_settings():
     assert settings.LOG_COLORS is True
     assert settings.DATABASE_URL.startswith("sqlite:///")
     assert settings.IMPORT_CHUNK_SIZE == 10_000
+    assert settings.API_HOST == "localhost"
+    assert settings.API_PORT == 8080
     assert settings.API_TRUST_PROXY_IPS == "127.0.0.1"
